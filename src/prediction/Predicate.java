d913e76676c72b07d1cd3b3bc83392aadd985793
mountain3th <mountainking@126.com>
2015-04-28 21:03:29 +0800

    add concept class

--- src/prediction/Predicate.java
+++ src/prediction/Predicate.java
@@ -1,7 +1,26 @@
 package prediction;
 
 public class Predicate {
-	public static void generate(int subject) {
+	
+	public static class Concept {
+		int object;
+		int confidency;
+		int depth;
+		
+		Concept(int o, int c, int d) {
+			this.object = o;
+			this.confidency = c;
+			this.depth = d;
+		}
+	}
+	
+	public static void generate() {
+		for(int index = 0; index < 10000; index++) {
+			generate(index);
+		}
+	}
+	
+	private static void generate(int subject) {
 		
 	}
 }
