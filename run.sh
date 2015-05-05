33cb6d12c1cb5de42be664133b1e68e9d70baa35
mountain3th <mountainking@126.com>
2015-05-05 21:00:19 +0800

    make run.sh more efficient

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-java -cp bin launcher.Launcher -debug -file dataset/graph.lg -support 1000
+#!/bin/bash
+inputfile = mappingbased_properties_en.ttl
+support_value = 1000
+python transform.py -i dataset/$1 -o dataset/graph.lg
+if [ $? == 0 ];then
+	echo "prepapre dataset ok"
+	java -cp bin launcher.Launcher -debug -file dataset/graph.lg -support $2 
+else;then
+	echo "prepapre dataset failed."
+fi
