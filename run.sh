8b1bd0dbf1a15c945e6411980ef69236e0a3e769
mountain3th <mountainking@126.com>
2015-05-07 10:40:32 +0800

    add some if judgement

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-inputfile = mappingbased_properties_en.ttl
-support_value = 1000
+if [ -z $1 ];then
+	echo "not all args set."
+	exit
+fi
 python transform.py -i dataset/$1 -o dataset/graph.lg
-if [ $? == 0 ];then
-	echo "prepapre dataset ok"
+if [ $? -eq 0 ];then
+	echo "prepapre dataset ok."
 	java -cp bin launcher.Launcher -debug -file dataset/graph.lg -support $2 
 else;then
 	echo "prepapre dataset failed."
