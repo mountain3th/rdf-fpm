5b5864a3a01ca5580198e658f4b3f039ebbf440c
mountain3th <mountainking@126.com>
2015-05-07 11:30:13 +0800

    dataset

--- run.sh
+++ run.sh
@@ -3,10 +3,10 @@ if [ $# != 5 ];then
 	echo "not all args set."
 	exit
 fi
-python transform.py -i $1 -o $2
+python transform.py -i dataset/$1 -o dataset/$2
 if [ $? -eq 0 ];then
 	echo "prepapre dataset ok."
-	java -cp bin launcher.Launcher -debug -file $2 -support $3 -confidence $4
+	java -cp bin launcher.Launcher -debug -file dataset/$2 -support $3 -confidence $4
 else
 	echo "prepapre dataset failed."
 fi
